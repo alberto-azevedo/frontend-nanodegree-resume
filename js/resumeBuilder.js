b9ee90ad8f05a8af7664aa960954f4d2545b3a93
Alberto Azevedo <Alberto Azevedo>
2017-05-04 15:41:17 -0300

    Fixing revision obs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,22 +19,39 @@ var bio = {
         //skills
         if (bio.skills.length > 0) {
             $("#header").append(HTMLskillsStart);
-            for (var skill in bio.skills) {
-                $("#skills").append(formatHtmlTemplate(HTMLskills, bio.skills[skill]));
-            }
+            bio.skills.forEach(function(skill) {
+              $("#skills").append(formatHtmlTemplate(HTMLskills, skill));
+              //console.log(skill);
+            })
         }
         //pic
         $("#header").prepend(formatHtmlTemplate(HTMLbioPic, bio.biopic));
         //top contacts
-        $("#topContacts").append(formatHtmlTemplate(HTMLmobile, this.contacts.mobile));
-        $("#topContacts").append(formatHtmlTemplate(HTMLemail, this.contacts.email));
-        $("#topContacts").append(formatHtmlTemplate(HTMLgithub, this.contacts.github));
+        $("#topContacts").append(formatHtmlTemplate(HTMLmobile,
+          this.contacts.mobile));
+        $("#topContacts").append(formatHtmlTemplate(HTMLemail,
+          this.contacts.email));
+        $("#topContacts").append(formatHtmlTemplate(HTMLgithub,
+          this.contacts.github));
+        $("#topContacts").append(formatHtmlTemplate(HTMLlocation,
+          this.contacts.location));
 
         //footer contacts -
-        $("#footerContacts").append(formatHtmlTemplate(HTMLmobile, this.contacts.mobile));
-        $("#footerContacts").append(formatHtmlTemplate(HTMLemail, this.contacts.email));
-        $("#footerContacts").append(formatHtmlTemplate(HTMLgithub, this.contacts.github));
+        $("#footerContacts").append(formatHtmlTemplate(HTMLmobile,
+          this.contacts.mobile));
+        $("#footerContacts").append(formatHtmlTemplate(HTMLemail,
+          this.contacts.email));
+        $("#footerContacts").append(formatHtmlTemplate(HTMLgithub,
+          this.contacts.github));
+        $("#footerContacts").append(formatHtmlTemplate(HTMLlocation,
+          this.contacts.location));
 
+        //generic info
+        var formattedRole = HTMLheaderRole.replace("%data%", "Web developer");
+        $("#header").prepend(formattedRole);
+
+        var formattedName = HTMLheaderName.replace("%data%", "Alberto Azevedo");
+        $("#header").prepend(formattedName);
     }
 };
 
@@ -44,33 +61,35 @@ var work = {
             "employer": "Banorte",
             "title": "Programmer",
             "location": "Rio de Janeiro",
-            "dates": ["1989", "1995"],
+            "dates": "1989, 1995",
             "description": "Bank"
         },
         {
             "employer": "Conecta",
             "title": "Analista",
             "location": "Sao Paulo",
-            "dates": ["1996", "2002"],
+            "dates": "1996, 2002",
             "description": "TI"
         }
     ],
     "position": "Analista",
     "display": function() {
-        //console.log("work display funct");
-        for (var job in work.jobs) {
-            $("#workExperience").append(HTMLworkStart);
-            var formattedEmployer = formatHtmlTemplate(HTMLworkEmployer, work.jobs[job].employer);
-            var formattedTitle = formatHtmlTemplate(HTMLworkTitle, work.jobs[job].title);
-            var formattedEmployerTitle = formattedEmployer + formattedTitle;
-            $(".work-entry:last").append(formattedEmployerTitle);
+        work.jobs.forEach(function(job) {
+          //console.log(job.employer);
+          $("#workExperience").append(HTMLworkStart);
+          var formattedEmployer = formatHtmlTemplate(HTMLworkEmployer,
+            job.employer);
+          var formattedTitle = formatHtmlTemplate(HTMLworkTitle,job.title);
+          var formattedEmployerTitle = formattedEmployer + formattedTitle;
+          $(".work-entry:last").append(formattedEmployerTitle);
 
-            var formattedDates = formatHtmlTemplate(HTMLworkDates, work.jobs[job].dates);
-            $(".work-entry:last").append(formattedDates);
+          var formattedDates = formatHtmlTemplate(HTMLworkDates, job.dates);
+          $(".work-entry:last").append(formattedDates);
 
-            var formattedDescription = formatHtmlTemplate(HTMLworkDescription, work.jobs[job].description);
-            $(".work-entry:last").append(formattedDescription);
-        }
+          var formattedDescription = formatHtmlTemplate(HTMLworkDescription,
+            job.description);
+          $(".work-entry:last").append(formattedDescription);
+        });
     }
 };
 
@@ -78,29 +97,34 @@ var work = {
 var projects = {
     "projects": [{
             "title": "GerIMP",
-            "dates": ["1991", "1992"],
-            "description": "Desenvolvimento de um Gerenciador de Impressoras para agencias SNA",
-            "images": []
+            "dates": "1991, 1992",
+            "description": "Desenvolvimento de um Gerenciador de Impressoras " +
+              "para agencias SNA",
+            "images": "[http://lorempixel.com/400/200/]"
         },
         {
             "title": "SNA",
-            "dates": ["1992", "1993"],
-            "description": "Conversão da comunicação das agencias de BSC3 para SNA",
-            "images": []
+            "dates": "1992, 1993",
+            "description": "Conversão da comunicação das agencias de BSC3 " +
+              "para SNA",
+            "images": "[http://lorempixel.com/400/200/]"
         }
     ]
 };
 
 projects.display = function() {
-    for (var project in projects.projects) {
-        $("#projects").append(HTMLprojectStart);
-        var formattedProjectTitle = formatHtmlTemplate(HTMLprojectTitle, projects.projects[project].title);
-        $(".project-entry:last").append(formattedProjectTitle);
-        var formattedDates = formatHtmlTemplate(HTMLprojectTitle, projects.projects[project].dates);
-        $(".project-entry:last").append(formattedDates);
-        var formattedProjectDescription = formatHtmlTemplate(HTMLprojectTitle, projects.projects[project].description);
-        $(".project-entry:last").append(formattedProjectDescription);
-    }
+    projects.projects.forEach(function(project) {
+      //console.log(project.title);
+      $("#projects").append(HTMLprojectStart);
+      var formattedProjectTitle = formatHtmlTemplate(HTMLprojectTitle,
+        project.title);
+      $(".project-entry:last").append(formattedProjectTitle);
+      var formattedDates = formatHtmlTemplate(HTMLprojectTitle, project.dates);
+      $(".project-entry:last").append(formattedDates);
+      var formattedProjectDescription = formatHtmlTemplate(HTMLprojectTitle,
+        project.description);
+      $(".project-entry:last").append(formattedProjectDescription);
+    });
 };
 
 
@@ -111,42 +135,43 @@ var education = {
         "location": "Recife",
         "degree": "BA",
         "majors": ["CompSci"],
-        "dates": ["1989", "1992"],
+        "dates": "1989, 1992",
         "url": ""
     }],
     "onlineCourses": [{
         "title": "Front Web Developer",
         "school": "Udacity",
-        "dates": ["2017"],
+        "dates": "2017",
         "url": "http://www.udacity.com"
     }],
     "city": "Recife",
     "display": function() {
 
         $("#education").append(HTMLschoolStart);
-        for (var school in education.schools) {
-            $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolName, education.schools[school].name));
-            $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolDates, education.schools[school].dates));
-            $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolLocation, education.schools[school].location));
-            $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolMajor, education.schools[school].majors));
-        }
+        education.schools.forEach(function(school) {
+        $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolName,
+          school.name));
+        $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolDates,
+          school.dates));
+        $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolLocation,
+          school.location));
+        $(".education-entry:last").append(formatHtmlTemplate(HTMLschoolMajor,
+          school.majors));
+        });
 
         $("#education").append(HTMLonlineClasses);
-        for (var onlineCourse in education.onlineCourses) {
-            var temp = formatHtmlTemplate(HTMLonlineTitle, education.onlineCourses[onlineCourse].title);
-            school = temp + formatHtmlTemplate(HTMLonlineSchool, education.onlineCourses[onlineCourse].school);
-            $("#education").append(school);
-            $("#education").append(formatHtmlTemplate(HTMLonlineDates, education.onlineCourses[onlineCourse].dates));
-            $("#education").append(formatHtmlTemplate(HTMLonlineURL, education.onlineCourses[onlineCourse].url));
-        }
+        education.onlineCourses.forEach(function(onlineCourse) {
+          var temp = formatHtmlTemplate(HTMLonlineTitle, onlineCourse.title);
+          school = temp + formatHtmlTemplate(HTMLonlineSchool,
+            onlineCourse.school);
+          $("#education").append(school);
+          $("#education").append(formatHtmlTemplate(HTMLonlineDates,
+             onlineCourse.dates));
+          $("#education").append(formatHtmlTemplate(HTMLonlineURL,
+            onlineCourse.url));
+        });
     }
 };
-
-var formattedRole = HTMLheaderRole.replace("%data%", "Web developer");
-$("#header").prepend(formattedRole);
-
-var formattedName = HTMLheaderName.replace("%data%", "Alberto Azevedo");
-$("#header").prepend(formattedName);
 
 projects.display();
 bio.display();
